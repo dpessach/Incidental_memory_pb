1a9a5807318de83ed20414efec611add79d256ce
singmann <henrik.singmann@psychologie.uni-freiburg.de>
2016-10-14 16:59:31 +0200

    updated text

--- script/texts_EN.js
+++ script/texts_EN.js
@@ -8,13 +8,13 @@ texts.warning = '<h2>Welcome</h2><p>To run this experiment we need launch into f
 
 texts.instructions = '<br><h2 style="text-align: center">Instructions</h2><p style="text-align: center">Thank you for \
 						taking part in our experiment! <br>\
-					  <p><br>In each run you will see six words presented sequentially. All words \
+					  <p><br>In this experiment your task is to make judgments on words. All words \
                       refer to concrete objects. Your task is to judge for each word \
                       whether the object it refers to is larger or smaller than a soccer ball:<ul><li>If the object is \
                       <strong>smaller</strong> than a soccer ball, please press the <strong>left</strong> arrow key.</li>\
                       <li>If the object is <strong>larger</strong> than a soccer ball, please press the \
-                      <strong>right</strong> arrow key.</li></ul><br><p>The trials will be organized into short runs. The time between successive trials will vary from one run to another. You will first do one practice run, followed by six serious runs.</p><p>Please try to make your judgement as quick and as accurate as possible. If you have three or more errors in one run, you will have to repeat it.</p>\
-                      <p>Please click on "next" to continue to the practice tral.</p>'
+                      <strong>right</strong> arrow key.</li></ul><br><p>The experiment will be organized into several short blocks of six trials. The time between successive trials will vary from one block to another. You will first do one practice block, followed by six serious blocks.</p><p>Please try to make your judgement as quickly and as accurately as possible. If you make three or more errors in one block, you will have to do a new block to replace it.</p>\
+                      <p>Please click on "next" to start with the practice block.</p>'
 
 texts.reminder = '<p>When you are ready for the practice trial, please press the right arrow key. If you \
                               want to re-read the instructions, press the left arrow key.</p><p>Reminder:<ul>\
@@ -46,7 +46,7 @@ texts.empty_error = '<p id="empty_error" style="color:red;"><p>'
 
 texts.button = "<div style='float:right;'><button id='jspsych-fullscreen-button'>Next</button></div>"
 
-texts.end_fullscreen = 'That was all! Now we kindly ask you to answer a few questions about yourself. In order to do that, the fullscreen mode will be terminated and you will be forwarded to the questionnaire.'
+texts.end_fullscreen = 'That was all! Now we kindly ask you to answer a few questions about yourself. In order to do that, fullscreen mode will be exited and you will be forwarded to the questionnaire.'
 
 
 						
@@ -54,12 +54,12 @@ texts.save = 'Here you can write any other comments or remarks on the experiment
 				be saved and you will recieve the code for the payment.'
 
 texts.debriefing = '<p><h2>Thank you again for participating in our experiment.</h2></p><p> The goal of this experiment was \
-                   to investigate how people unintentionally remember information, \
+                   to investigate how people remember information they were not instructed to remember \
                    and whether this is accomplished better or worse when more time is available. To \
-                   that end we tested your memory for the six words of the last trial. \
-                   We varied the free time following each of the words - either only \
+                   that end we tested your memory for the six words of the last block. \
+                   We varied the free time following each of the words between blocks - either only \
                    100 ms or 5000 ms after each word. We expect that participants who had more \
                    time following the words are able to remember them better.</p><p>Please do not share \
-                   this information with anyone! It is crucial the explanation is kept confidential until we \
+                   this information with anyone! It is crucial that the explanation is kept confidential until we \
                    have collected all data.</p><p>Here is your code to confirm your participation: IncMem_73_1</p>\
                    <p>You can now close the browser window/tab after copying the confirmation code.</p>'
